4ede7a1d2171e289c92421178f0b74f8c619ba5c
fyw35855-web <fyw35855@gmail.com>
2026-07-28 11:36:25 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,11 +1,16 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
+# اسم ومسار قاعدة البيانات (راح ينشئ ملف اسمه supermarket.db بنفس المجلد)
 SQLALCHEMY_DATABASE_URL = "sqlite:///./supermarket.db"
+
+# إعداد المحرك، و (check_same_thread=False) ضرورية جداً حتى FastAPI يكدر يقرا ويكتب براحته
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
+# دالة لفتح وإغلاق الاتصال بقاعدة البيانات مع كل طلب يوصل للسيرفر
 def get_db():
     db = SessionLocal()
     try:
